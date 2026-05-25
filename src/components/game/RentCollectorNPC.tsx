62fd80cdd9c85fb8f776d6b6592b6b934d5b08fa
fernandorosero91 <elier.rosero@campusucc.edu.co>
2026-05-25 18:33:48 -0500

    mejoras posicion npc renta en nivels 5 y 6

--- src/components/game/RentCollectorNPC.tsx
+++ src/components/game/RentCollectorNPC.tsx
@@ -144,6 +144,7 @@ export function RentCollectorNPC() {
       ref={group}
       position={(() => {
         const level = useGameStore.getState().currentLevel;
+        if (level >= 5) return [4, 0.3, 1] as [number, number, number];
         if (level >= 4) return [15, 0.3, -10] as [number, number, number];
         if (level >= 3) return [5, 0.3, 2] as [number, number, number];
         if (level >= 2) return [3, 0.3, 5] as [number, number, number];
